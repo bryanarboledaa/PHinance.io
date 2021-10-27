e9c681530398a113da6672e741f4af996695dc76
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-28 02:57:20 +0800

    new logic for creating stripe customer id, generated once user has a success checkout

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -1,16 +1,33 @@
 class CheckoutsController < ApplicationController
+  after_action :set_stripe_customer_id, only: [:create]
+
   def create
-    @session = Stripe::Checkout::Session.create({ customer: current_user.stripe_customer_id,
-      success_url: root_url,
+    @session = Stripe::Checkout::Session.create(
+      {
+        success_url: root_url,
       cancel_url: pricing_url,
       payment_method_types: ['card'],
       line_items: [
         { price: params[:price], quantity: 1 }
       ],
-      mode: 'subscription' })
+      mode: 'subscription'
+      }
+    )
 
     respond_to do |format|
       format.js
     end
+  end
+
+  private
+
+  def to_s
+    email
+  end
+
+  def set_stripe_customer_id
+    @users = User.all
+    customer = Stripe::Customer.create(email: current_user.email)
+    current_user.update(stripe_customer_id: customer.id)
   end
 end
