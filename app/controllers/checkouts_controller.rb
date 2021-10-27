36491d84f1961a0d4f8a6280a883e84abf2b12c5
mumble-07 <andreapauline.deguzman@gmail.com>
2021-10-28 04:19:17 +0800

    added condition for stripe not to create an id every session

--- app/controllers/checkouts_controller.rb
+++ app/controllers/checkouts_controller.rb
@@ -3,15 +3,14 @@ class CheckoutsController < ApplicationController
 
   def create
     @session = Stripe::Checkout::Session.create(
-      {
+      { customer: current_user.stripe_customer_id,
         success_url: root_url,
       cancel_url: pricing_url,
       payment_method_types: ['card'],
       line_items: [
         { price: params[:price], quantity: 1 }
       ],
-      mode: 'subscription'
-      }
+      mode: 'subscription' }
     )
 
     respond_to do |format|
@@ -27,6 +26,8 @@ class CheckoutsController < ApplicationController
 
   def set_stripe_customer_id
     @users = User.all
+    return unless current_user.stripe_customer_id.nil?
+
     customer = Stripe::Customer.create(email: current_user.email)
     current_user.update(stripe_customer_id: customer.id)
   end
