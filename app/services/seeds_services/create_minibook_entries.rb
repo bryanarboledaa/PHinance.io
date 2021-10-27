db778e07cbe1b9cf2d6a61371771aa9b1a5f3d8a
Bryan Arboleda <jbdarboleda@gmail.com>
2021-10-27 23:57:11 +0800

    B/Hotfix - Updated User model, added more User rspec tests and updated service seeds

--- app/services/seeds_services/create_minibook_entries.rb
+++ app/services/seeds_services/create_minibook_entries.rb
@@ -7,7 +7,7 @@ module SeedsServices
       @minibook = MiniBook.all
       @minibook.each do |book|
         entry_count = 0
-        5.times do
+        3.times do
           MinibookEntry.create(mini_book_id: book.id, date_minib_entry: Time.zone.today, minibook_entry_description: "Payment for something#{entry_count}", or_vat_reg_tin_minib: 'asdf345678', debit_minib: 50_000.00, credit_minib: 30_000.00, minibook_balance: 20_000.00)
           entry_count += 1
         end
