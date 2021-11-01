15c808829b75edea021899adfdf4634e0f9b8d19
mumble-07 <andreapauline.deguzman@gmail.com>
2021-11-02 01:53:43 +0800

    updated mini_books with association to user and minibook_entries

--- app/controllers/mini_books_controller.rb
+++ app/controllers/mini_books_controller.rb
@@ -6,6 +6,10 @@ class MiniBooksController < ApplicationController
     @mini_books = MiniBook.all
   end
 
+  def show
+    @mini_book = MiniBook.find(params[:id])
+  end
+
   def new
     @mini_book = current_user.mini_books.build
   end
@@ -13,7 +17,7 @@ class MiniBooksController < ApplicationController
   def create
     @mini_book = current_user.mini_books.build(mini_book_params)
     if @mini_book.save
-      redirect_to minibooks_path, notice: 'Product was successfully created.'
+      redirect_to mini_books_path, notice: 'Product was successfully created.'
     else
       render :new
     end
@@ -27,7 +31,7 @@ class MiniBooksController < ApplicationController
     @mini_book = MiniBook.find(params[:id])
     if @mini_book.update(mini_book_params)
       flash[:notice] = 'Sucessfully updated MiniBook'
-      redirect_to minibooks_path
+      redirect_to mini_books_path
     else
       render :edit
     end
@@ -37,15 +41,15 @@ class MiniBooksController < ApplicationController
     @mini_book = MiniBook.find(params[:id])
     if @mini_book.destroy
       flash[:notice] = 'Sucessfully deleted Mini Book'
-      redirect_to minibooks_path
+      redirect_to mini_books_path
     else
-      render :minibooks_path
+      render :mini_books_path
     end
   end
 
   def correct_user
     @mini_book = current_user.mini_books.find_by(id: params[:id])
-    redirect_to minibooks_path, notice: 'Not Authorized to access this page' if @mini_book.nil?
+    redirect_to mini_books_path, notice: 'Not Authorized to access this page' if @mini_book.nil?
   end
 
   private
