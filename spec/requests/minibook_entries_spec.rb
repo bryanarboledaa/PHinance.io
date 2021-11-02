9e839ce4007a61257fca759f3524bf2f879c179a
mumble-07 <andreapauline.deguzman@gmail.com>
2021-11-02 17:30:05 +0800

    added more spec request test for mainbook,minibook, and minibook entries

--- spec/requests/minibook_entries_spec.rb
+++ spec/requests/minibook_entries_spec.rb
@@ -1,49 +1,86 @@
 require 'rails_helper'
 
 RSpec.describe 'MinibookEntries', type: :request do
-  let(:confirmed_user) { create(:confirmed_user) }
-  let(:minibook_entries) { create(:minibook_entries) }
+  let!(:confirmed_user) { create(:confirmed_user) }
+  let!(:minibook) { MiniBook.create(user_id: confirmed_user.id, minibook_name: 'Avion School') }
+  let!(:minibook_entry) { MinibookEntry.create(mini_book_id: minibook.id, user_id: confirmed_user.id, date_minib_entry: DateTime.now, minibook_entry_description: 'Payment for something', or_vat_reg_tin_minib: 'tin12345', debit_minib: 1000, credit_minib: 500) }
 
   describe 'confirmed user gets to access minibook entries page' do
     before do
       sign_in confirmed_user
     end
 
-    it 'returns the minibook entries page' do
-      get minibook_entries_path
+    it 'returns the /mini_books/:mini_book_id/minibook_entries page' do
+      get mini_book_minibook_entries_path(minibook_entry.mini_book_id)
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders the minibook entries page' do
-      get minibook_entries_path
+      get mini_book_minibook_entries_path(minibook_entry.mini_book_id)
       expect(response).to render_template('index')
     end
 
-    it 'returns the list of all minibook entries' do
-      get all_minibook_entries_path
-      expect(response).to have_http_status(:ok)
-    end
-
     it 'renders the page list of all minibook entries' do
-      get all_minibook_entries_path
-      expect(response).to render_template('all_minibook_entries')
+      get mini_book_minibook_entries_path(minibook_entry.mini_book_id)
+      expect(response).to render_template('_all_minibook_entries')
     end
   end
 
   describe 'User gets to create a minibook entry' do
     before do
       sign_in confirmed_user
-      create(:minibook_entries)
     end
 
     it 'direct users to minibook entry form' do
-      get new_minibook_entry_path
+      get new_mini_book_minibook_entry_path(minibook_entry.mini_book_id)
       expect(response).to have_http_status(:ok)
     end
 
     it 'renders minibook entry form' do
-      get new_minibook_entry_path
+      get new_mini_book_minibook_entry_path(minibook_entry.mini_book_id)
       expect(response).to render_template('new')
+    end
+
+    it 'allows user to post create minibook entry' do
+      post mini_book_minibook_entries_path(minibook_entry.mini_book_id), params: { minibook_entry: { mini_book_id: minibook.id, user_id: confirmed_user.id, date_minib_entry: DateTime.now, minibook_entry_description: 'Payment for something', or_vat_reg_tin_minib: 'tin12345', debit_minib: 1000, credit_minib: 500 } }
+      expect(response).to redirect_to mini_book_minibook_entries_path(minibook_entry.mini_book_id)
+    end
+  end
+
+  describe 'User gets to edit a minibook entry' do
+    before do
+      sign_in confirmed_user
+    end
+
+    it 'renders minibook edit entry form' do
+      get edit_mini_book_minibook_entry_path(minibook_entry.mini_book_id, minibook_entry.id)
+      expect(response).to render_template('edit')
+    end
+
+    it 'allows user to edit minibook entry' do
+      get edit_mini_book_minibook_entry_path(minibook_entry.mini_book_id, minibook_entry.id), params: { minibook_entry: { mini_book_id: minibook.id, user_id: confirmed_user.id, date_minib_entry: DateTime.now, minibook_entry_description: 'Payment for something', or_vat_reg_tin_minib: 'tin12345', debit_minib: 1000, credit_minib: 500 } }
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'allows user to update minibook entry' do
+      put mini_book_minibook_entry_path(minibook_entry.mini_book_id, minibook_entry.id), params: { minibook_entry: { mini_book_id: minibook.id, user_id: confirmed_user.id, date_minib_entry: DateTime.now, minibook_entry_description: 'Payment for something', or_vat_reg_tin_minib: 'tin12345', debit_minib: 1000, credit_minib: 500 } }
+      expect(response).to redirect_to mini_book_minibook_entries_path(minibook_entry.mini_book_id)
+    end
+
+    it 'allows user to update/patch minibook entry' do
+      patch mini_book_minibook_entry_path(minibook_entry.mini_book_id, minibook_entry.id), params: { minibook_entry: { mini_book_id: minibook.id, user_id: confirmed_user.id, date_minib_entry: DateTime.now, minibook_entry_description: 'Payment for something', or_vat_reg_tin_minib: 'tin12345', debit_minib: 1000, credit_minib: 500 } }
+      expect(response).to redirect_to mini_book_minibook_entries_path(minibook_entry.mini_book_id)
+    end
+  end
+
+  describe 'User gets to delete a minibook entry' do
+    before do
+      sign_in confirmed_user
+    end
+
+    it 'allows user to delete a minibook entry' do
+      delete mini_book_minibook_entry_path(minibook_entry.mini_book_id, minibook_entry.id), params: { minibook_entry: { mini_book_id: minibook.id, user_id: confirmed_user.id, date_minib_entry: DateTime.now, minibook_entry_description: 'Payment for something', or_vat_reg_tin_minib: 'tin12345', debit_minib: 1000, credit_minib: 500 } }
+      expect(response).to redirect_to mini_book_minibook_entries_path(minibook_entry.mini_book_id)
     end
   end
 end
