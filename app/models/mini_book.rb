ab0b0b60f01b2d1c196e86dc2d6698352e7c1c2f
mumble-07 <andreapauline.deguzman@gmail.com>
2021-11-02 17:28:49 +0800

    added validations for model rspec testing

--- app/models/mini_book.rb
+++ app/models/mini_book.rb
@@ -2,5 +2,6 @@ class MiniBook < ApplicationRecord
   has_many :minibook_entries, dependent: :destroy
   belongs_to :user
   # has_many :checkouts #, dependent: :destroy
+  validates :user_id, presence: true
   validates :minibook_name, presence: true
 end
