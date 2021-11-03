44929a02e72ea6aa9f536df13a643eb129dfcec8
mumble-07 <andreapauline.deguzman@gmail.com>
2021-11-03 23:32:51 +0800

    helper for computing total balances

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,13 @@
 module ApplicationHelper
   def calculate_balance(debit, credit)
-    total__balance = debit - credit
-    number_to_currency(total__balance, unit: ' PHP ')
+    debit ||= 0
+    credit ||= 0
+    (debit - credit).round(4)
+  end
+
+  def calculate_total_balance(running_total, item_balance)
+    running_total ||= 0
+    item_balance ||= 0
+    (running_total + item_balance).round(4)
   end
 end
