3bfb0bb6530272e7125436b90e6b7640ca43d119
mumble-07 <andreapauline.deguzman@gmail.com>
2021-11-04 00:08:19 +0800

    added comutation helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,16 @@ module ApplicationHelper
     item_balance ||= 0
     (running_total + item_balance).round(4)
   end
+
+  def calculate_total_debit(running_total_debit, credit)
+    running_total_debit ||= 0
+    credit ||= 0
+    (running_total_debit + credit).round(4)
+  end
+
+  def calculate_total_credit(running_total_credit, credit)
+    running_total_credit ||= 0
+    credit ||= 0
+    (running_total_credit + credit).round(4)
+  end
 end
