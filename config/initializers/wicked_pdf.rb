2eb98dab85b62386099908955c50320d90065cd7
mumble-07 <andreapauline.deguzman@gmail.com>
2021-11-05 01:00:43 +0800

    config for wicked pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -2,5 +2,7 @@ WickedPdf.config ||= {}
 WickedPdf.config.merge!({
   layout: 'pdf',
   orientation: 'Landscape', 
-  page_size: 'A4'
+  page_size: 'A4',
+  background: true,
+  print_media_type: true
 }) 
