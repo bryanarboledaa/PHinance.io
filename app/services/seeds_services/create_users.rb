fa2b37d2440b26ce1a1633581df69c1b28df5e89
mumble-07 <andreapauline.deguzman@gmail.com>
2021-11-05 16:28:37 +0800

    seeds for demo later

--- app/services/seeds_services/create_users.rb
+++ app/services/seeds_services/create_users.rb
@@ -7,7 +7,7 @@ module SeedsServices
       @logger.info 'Deleted all data from User model'
       user_count = 0
       15.times do
-        user = User.create(email: "#{Faker::Internet.email}+#{user_count}", password: "abc12345#{user_count}", full_name: Faker::Name.unique.name, username: "abctech_#{user_count}", company: Faker::Company.name, status: [0, 1].sample)
+        user = User.create(email: Faker::Internet.email.to_s, password: 'abc12345', full_name: Faker::Name.unique.name, username: "abctech_#{user_count}", company: Faker::Company.name, status: [0, 1].sample)
         user_count += 1
         user.skip_confirmation!
         user.save!
